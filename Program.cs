4852161b76b12155b2b7ca4ced588acda6ab8dc8
Maxim Sorokin <maximka773@mail.ru>
2022-10-23 10:12:29 +0300

    задача 24

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+
+if (!isParsed)
+{
+    Console.WriteLine("Ошибка! Не правильно введены данные");
+    return;
+}
+
+if (a < 1)
+{
+    Console.WriteLine("0");
+    return;
+}
+
+int sum = GetSumNumbers(a);
+
+Console.WriteLine(sum);
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
