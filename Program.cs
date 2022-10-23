e5907945cf492433c41c9551704b5cbbda7d2053
Maxim Sorokin <maximka773@mail.ru>
2022-10-23 17:20:02 +0300

    все

--- Program.cs
+++ Program.cs
@@ -1,36 +1,42 @@
-﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+﻿Console.Clear();
+Console.WriteLine("Введите номер задачи: 24, 26, 28 или 30");
+int z = Convert.ToInt32(Console.ReadLine());
 
-// bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
 
-// if (!isParsed)
-// {
-//     Console.WriteLine("Ошибка! Не правильно введены данные");
-//     return;
-// }
+if (z == 24)
+{
+    bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
 
-// if (a < 1)
-// {
-//     Console.WriteLine("0");
-//     return;
-// }
+    if (!isParsedA)
+    {
+        Console.WriteLine("Ошибка! Не правильно введены данные");
+        return;
+    }
 
-// int sum = GetSumNumbers(a);
+    if (a < 1)
+    {
+        Console.WriteLine("0");
+        return;
+    }
 
-// Console.WriteLine(sum);
+    int sum = GetSumNumbers(a);
 
-// int GetSumNumbers(int number)
-// {
-//     int sum = 0;
+    Console.WriteLine(sum);
+}
 
-//     for (int i = 1; i <= number; i++)
-//     {
-//         sum += i;
-//     }
-//     return sum;
-// }
+// метод для задачи 28 - находит сумму чисел от 1 до заданного
 
+int GetSumNumbers(int number)
+{
+    int sum = 0;
 
-
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
 
 
 
@@ -38,29 +44,33 @@
 // Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
 // 456 -> 3 78 -> 2 89126 -> 5
 
-// bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+if (z == 26)
+{
+    bool isParsedNum = int.TryParse(Console.ReadLine(), out int num);
 
-// if (!isParsed)
-// {
-//     Console.WriteLine("Ошибка! Не правильно введены данные");
-//     return;
-// }
+    if (!isParsedNum)
+    {
+        Console.WriteLine("Ошибка! Не правильно введены данные");
+        return;
+    }
 
-// int count = CountDigit(a);
-// Console.WriteLine(count);
+    int count = CountDigit(num);
+    Console.WriteLine(count);
+}
 
+// метод для задачи 26 - находит количество цифр в числе
 
-// int CountDigit(int number)
-// {
-//     int count;
+int CountDigit(int number)
+{
+    int count;
 
-//     for (count = 0; number != 0; count++)
-//     {
-//         number /= 10;
-//     }
+    for (count = 0; number != 0; count++)
+    {
+        number /= 10;
+    }
 
-//     return count;
-// }
+    return count;
+}
 
 
 
@@ -74,34 +84,39 @@
 // Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 // 4->24 5->120
 
-// bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+if (z == 28)
+{
+    bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
 
-// if (!isParsedN)
-// {
-//     Console.WriteLine("Ошибка! Не правильно введены данные");
-//     return;
-// }
+    if (!isParsedN)
+    {
+        Console.WriteLine("Ошибка! Не правильно введены данные");
+        return;
+    }
 
-// if (n < 1)
-// {
-//     Console.WriteLine("0");
-//     return;
-// }
+    if (n < 1)
+    {
+        Console.WriteLine("0");
+        return;
+    }
 
-// int comp = GetCompositionNumbers(n);
+    int comp = GetCompositionNumbers(n);
 
-// Console.WriteLine(comp);
+    Console.WriteLine(comp);
+}
 
-// int GetCompositionNumbers(int number)
-// {
-//     int composition = 1;
+// метод для задачи 28 - находит произведение чисел от 1 до заданного
 
-//     for (int i = 1; i <= number; i++)
-//     {
-//         composition *= i;
-//     }
-//     return composition;
-// }
+int GetCompositionNumbers(int number)
+{
+    int composition = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        composition *= i;
+    }
+    return composition;
+}
 
 
 
@@ -110,23 +125,28 @@
 // Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-Console.WriteLine("Введите размер массива");
-bool isParsedS = int.TryParse(Console.ReadLine(), out int s);
-
-Console.WriteLine("Введите левую границу чисел");
-bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
-
-Console.WriteLine("Введите правую границу чисел");
-bool isParsedB = int.TryParse(Console.ReadLine(), out int b);
-
-if (!isParsedS || !isParsedA || !isParsedB)
+if (z == 30)
 {
-    Console.WriteLine("Ошибка! Не правильно введены данные");
-    return;
+    Console.WriteLine("Введите размер массива");
+    bool isParsedS = int.TryParse(Console.ReadLine(), out int s);
+
+    Console.WriteLine("Введите левую границу чисел");
+    bool isParsedL = int.TryParse(Console.ReadLine(), out int l);
+
+    Console.WriteLine("Введите правую границу чисел");
+    bool isParsedR = int.TryParse(Console.ReadLine(), out int r);
+
+    if (!isParsedS || !isParsedL || !isParsedR)
+    {
+        Console.WriteLine("Ошибка! Не правильно введены данные");
+        return;
+    }
+
+    int[] array = GetRandomArray(s, l, r);
+    PrintArray(array);
 }
 
-int[] array = GetRandomArray(s, a, b);
-PrintArray(array);
+// метод для задачи 30 - получение массива заданного размера из случайных чисел в заданном интервале
 
 int[] GetRandomArray(int size, int borderL, int borderR)
 {
@@ -139,6 +159,8 @@ int[] GetRandomArray(int size, int borderL, int borderR)
     }
     return array;
 }
+
+// метод для задачи 30 - выводит (распечатывает) массив в консоль
 
 void PrintArray(int[] array)
 {
